379e89b66e22ae25204f14fdcd9f3c7a56340b18
Zach Strader <zstrader@csumb.edu>
2018-04-08 20:49:08 -0700

    image test

--- src/main/java/com/example/microservicehw/DemoApplication.java
+++ src/main/java/com/example/microservicehw/DemoApplication.java
@@ -1,10 +1,20 @@
 package com.example.microservicehw;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.microservicehw.kittens.webv.KittensWeb;
 
 @Controller
 @SpringBootApplication
@@ -12,8 +22,9 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	@ResponseBody
-	String home() {
-		return "Helo wrkd";
+	BufferedImage home() throws MalformedURLException, IOException {
+		BufferedImage image = ImageIO.read(new URL("https://media1.giphy.com/media/Ei0mEO9NbtgVa/giphy-downsized-small.mp4"));
+		return image;
 	}
 	
 	public static void main(String[] args) {
