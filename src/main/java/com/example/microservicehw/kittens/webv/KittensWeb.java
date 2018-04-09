d7af4e7da4b16f75b5d850a0ef32183c4718cd69
Zach Strader <zstrader@csumb.edu>
2018-04-08 20:16:58 -0700

    commit attempt

--- src/main/java/com/example/microservicehw/kittens/webv/KittensWeb.java
+++ src/main/java/com/example/microservicehw/kittens/webv/KittensWeb.java
@@ -2,7 +2,7 @@ package com.example.microservicehw.kittens.webv;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +12,19 @@ import com.example.microservicehw.kittens.logic.KittensLogicManager;
 @RestController
 public class KittensWeb {
 	@Autowired
+	@Value("${}KittensApiUrl:")
+	private String KittensApiUrl;
+	@Value("${}KittensApiKey:")
+	private String KittensApiKey;
+	@Value("${}KittensApiTag:")
+	private String KittensApiTag;
 	KittensLogicManager manager;
 	@RequestMapping("/car")
 	@ResponseBody
-	ResponseEntity<String> kittens() {
-		//KittensWebResult kwrst = new KittensWebResult();
-		ResponseEntity<String> url = manager.obtainApiImageUrl();
-		//kwrst.setApiUrl(url);
-		return url;
+	KittensWebResult kittens() {
+		KittensWebResult kwrst = new KittensWebResult();
+		//ResponseEntity<String> url = manager.obtainApiImageUrl();
+		kwrst.setApiUrl(KittensApiUrl);
+		return kwrst;
 	}
 }
